2fa35a41d943719f726b86728f78f9c70dcc8d8d
subreto roy <47276704+subreto-roy@users.noreply.github.com>
2022-04-09 23:43:28 +0600

    Add files via upload

---
+++ src/main/java/ProtectedClass.java
@@ -0,0 +1,12 @@
+
+package newpackage1;
+
+import newpackage.withdraw;
+
+
+public class ProtectedClass extends withdraw {
+    public static void main(String[] args) {
+        ProtectedClass ob = new ProtectedClass();
+        ob.CheckBalance1();
+    }
+}
